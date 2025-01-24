614be2d01d3658ce60c5f8d64c6bcd27205dc4a7
54toshi <julianeterribla@protonmail.com>
2025-01-24 16:34:34 +0100

    feat: folderstructure

---
+++ proxyscraper.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import aiohttp
+import asyncio
+from aiohttp_socks import ProxyConnector
+from aiohttp import ClientTimeout
+
+class ProxyScraper:
+    def __init__(self, max_concurrent_checks: int = 50):
+        self.http_proxy_sources = [
+
+        ]
+        self.socks4_proxy_sources = [
+
+        ]
+        self.socks5_proxy_sources = [
+
+        ]
+        self.timeout_secs = 5
+        self.timeout = ClientTimeout(total=self.timeout_secs)
+        self.semaphore = asyncio.Semaphore(max_concurrent_checks)
+    async def getHTTP(self, http_proxy_sources):
+        pass
+    async def getSOCKS4(self, socks4_proxy_sources):
+        pass
+    async def getSOCKS5(self, socks5_proxy_sources):
+        pass
+
+    async def collect_proxies(self):
+        """
+        Collect all proxies from URLs and check their usability.
+        """
+        try:
+            tasks = []
+            proxies = [proxy.strip() for url in self.http_proxy_urls for proxy in requests.get(url).text.splitlines()]
+
+            # Create tasks for each proxy
+            tasks = [self.check_proxy(proxy_server=proxy) for proxy in proxies]
+
+            # Run tasks in parallel
+            await asyncio.gather(*tasks)
+
+            return self.working_proxies
+        except Exception as e:
+            print(f"Error in proxy collection: {e}")
+            return self.working_proxies
+
---
+++ proxystore.py
@@ -0,0 +1,10 @@
+import json
+import aiohttp
+import asyncio
+from aiohttp_socks import ProxyConnector
+from aiohttp import ClientTimeout
+
+class ProxyStore:
+    def __init__(self):
+        pass
+    pass
