d6fd83355f85908f4f678b6a79b08c5b6b60de1f
Aryt3 <Jxkobus@protonmail.com>
2025-01-25 03:23:04 +0100

    Adding Proxy-Scraping on initiation && Call of Validator after proxies have successfully been written to db

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from validators.main import ProxyValidator
 from scrapers.main import ProxyScraper
-from stores.main import ProxyStore
 import asyncio
 from pathlib import Path
 
 parent_directory = Path(__file__).parent
 
 if __name__ == '__main__':
-    #proxy_checker = ProxyValidator(max_concurrent_checks=50)
-    #results = asyncio.run(proxy_checker.collect_proxies())
-    pass
+    asyncio.run(ProxyScraper().run_scrapers())
